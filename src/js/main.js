93d494708225c71787e03607170440c256a7002f
Dmitry Romanov <from.module.import@gmail.com>
2023-02-05 18:11:59 +0300

    price section

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 $(function () {
-  $('.bike-slider').slick({
+  $('.bike-slider, .slider__items').slick({
     arrows: false,
     dots: true,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 2000,
   });
 });
